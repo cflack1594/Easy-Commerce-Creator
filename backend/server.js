fde4fe192ac80716e7d30854978b2cf39da2c573
cflack1594 <cflack1594@gmail.com>
2020-06-17 18:53:31 -0500

    Filled in missing get script in server so server now boots on to port 80

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,12 @@ import express from "express";
 const app = express();
 const PORT = 80;
 
-app.get();
+app.get("/", (_, res) => "<p>BEEP BOOP. Server is On. I AM A ROBOT</p>");
 
 app.listen(PORT, () => {
   console.log("running on port" + PORT);
 });
 
 (async () => {
-  console.log();
+  console.log("BEEP BOOP");
 })();
