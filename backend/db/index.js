d4669f5d547884102c50668fc17e3cb491919970
cflack1594 <cflack1594@gmail.com>
2020-06-17 18:25:45 -0500

    Add database call to retrieve data from sales and products collections

---
+++ backend/db/index.js
@@ -0,0 +1,17 @@
+import { client } from "./client";
+
+export const getCollection = async (collectionName) => {
+  try {
+    await client.connect();
+    const cursor = await client
+      .db("ecommerce-site-demo")
+      .collection(collectionName)
+      .find();
+
+    return await cursor.toArray();
+  } catch (e) {
+    throw new Error(e);
+  } finally {
+    await client.close();
+  }
+};
