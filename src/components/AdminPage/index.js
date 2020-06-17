cea97a4ab62975953c4df438ab859dbd0d8a0d8e
cflack1594 <cflack1594@gmail.com>
2020-06-17 18:30:09 -0500

    Initialize directory for admin page and some todos

---
+++ src/components/AdminPage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class AdminPage extends React.Component {
+  //Add product form
+  //other DB manipulation tools
+  //order fulfillment
+  //sales tracking
+
+  render() {
+    return <p>BEEP BOOP I AM A ROBOT</p>;
+  }
+}
