b3600da2187964b535924489531e7ac975d890d2
cflack1594 <cflack1594@gmail.com>
2020-06-17 18:26:13 -0500

    Add very basic express server

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 80;
+
+app.get();
+
+app.listen(PORT, () => {
+  console.log("running on port" + PORT);
+});
+
+(async () => {
+  console.log();
+})();
