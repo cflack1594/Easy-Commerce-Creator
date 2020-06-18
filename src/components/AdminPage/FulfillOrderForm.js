b465a42ab196143b30eb1c01ec585151e542d071
cflack1594 <cflack1594@gmail.com>
2020-06-18 15:42:25 -0500

    Add order fulfillment functionallity that fulfills orders and saves the data from each order

--- src/components/AdminPage/FulfillOrderForm.js
+++ src/components/AdminPage/FulfillOrderForm.js
@@ -1,16 +1,67 @@
 import React from "react";
-import { FormInput } from "./FormInput";
-import { SubmitButton } from "./SubmitButton";
-
 export class FulfillOrderForm extends React.Component {
-  inputKeys = { orderId: 1, name: "new order", type: "object" };
+  state = {
+    orders: [
+      {
+        orderID: 1,
+        value: "BEEP",
+        saleData: {},
+      },
+      {
+        orderID: 2,
+        value: "BOOP",
+        saleData: {},
+      },
+    ],
+
+    sales: [],
+  };
+
+  handleClick = (order, orders) => {
+    this.updateOrders(order.orderID, orders);
+    this.updatedSales(order);
+  };
+
+  //uses the orderID key to find the order within the state array
+  updateOrders = (orderID, orders) => {
+    const updatedOrders = orders.filter((order) => order.orderID !== orderID);
+    this.setState({ orders: updatedOrders });
+  };
+
+  updatedSales = (order) => {
+    const updatedSales = this.state.sales.concat(order);
+    this.setState({ sales: updatedSales });
+  };
+
+  createOrderForms = (orders) => {
+    return orders.length ? (
+      orders.map((order, index) => (
+        <form id={order.orderID} key={index}>
+          <input
+            type="string"
+            id="newOrder"
+            name="newOrder"
+            value={order.value}
+            readOnly
+          ></input>
+          <button
+            id="fulfillOrder"
+            name="fulfillOrder"
+            onClick={(e) => {
+              e.preventDefault();
+              this.handleClick(order, orders);
+            }}
+          >
+            Fulfill Order
+          </button>
+        </form>
+      ))
+    ) : (
+      <p>No Orders to complete</p>
+    );
+  };
 
   render() {
-    return (
-      <form>
-        <FormInput inputKeys={this.inputKeys} />
-        <SubmitButton />
-      </form>
-    );
+    return this.createOrderForms(this.state.orders);
   }
 }
