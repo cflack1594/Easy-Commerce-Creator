34bfefc639e3dc9ea7b68d8c6783117ff93e2122
cflack1594 <cflack1594@gmail.com>
2020-06-18 22:17:42 -0500

    Add client side api call for sales data

--- src/api/index.js
+++ src/api/index.js
@@ -8,3 +8,12 @@ export const getProducts = async () => {
     throw new Error(e);
   }
 };
+
+export const getSales = async () => {
+  try {
+    const resp = await window.fetch("http://localhost:3001/api/sales/sales");
+    return resp.json();
+  } catch (e) {
+    throw new Error(e);
+  }
+};
