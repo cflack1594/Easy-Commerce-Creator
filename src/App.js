a0c2b2b7a91968044643948d009a5a17210f9371
cflack1594 <cflack1594@gmail.com>
2020-06-20 18:44:37 -0500

    Refactor redundant functions to use a variable parameter instead of redundancy

--- src/App.js
+++ src/App.js
@@ -6,47 +6,98 @@ import * as api from "api";
 //add state proxy for components to work with
 export class App extends React.Component {
   state = {
+    auth: [],
     products: [],
+    orders: [],
     sales: [],
+    loggedIn: false,
   };
 
   async componentDidMount() {
-    const products = this.state.products.concat(await this.getProducts());
-    const sales = this.state.sales.concat(await this.getSales());
-    this.setState({ products: products, sales: sales });
+    this.setState({
+      products: await this.getData(
+        "http://localhost:3001/api/products/products"
+      ),
+      sales: await this.getData("http://localhost:3001/api/sales/sales"),
+      auth: await this.getData("http://localhost:3001/api/auth/auth"),
+    });
   }
 
-  getSales = async () => {
+  getData = async (url) => {
     try {
-      const resp = await api.getSales();
+      const resp = await api.getData(url);
       return resp;
     } catch (e) {
-      throw new Error(e);
+      throw Error(e);
     }
   };
 
-  getProducts = async () => {
-    try {
-      const resp = await api.getProducts();
-      return resp;
-    } catch (e) {
-      throw new Error(e);
-    }
-  };
+  // getSales = async () => {
+  //   try {
+  //     const resp = await api.getSales();
+  //     return resp;
+  //   } catch (e) {
+  //     throw new Error(e);
+  //   }
+  // };
+
+  // getProducts = async () => {
+  //   try {
+  //     const resp = await api.getProducts();
+  //     return resp;
+  //   } catch (e) {
+  //     throw new Error(e);
+  //   }
+  // };
 
   addProduct = (newProduct) => {
-    const products = this.state.products.concat(newProduct);
-    this.setState({ products: products });
+    this.setState({ products: [...this.state.products, newProduct] });
   };
+
+  addOrder = (newOrder) => {
+    this.setState({ orders: [...this.state.orders, ...newOrder] });
+  };
+
+  //uses the orderID key to find and remove the fulfilled order within the state array
+
+  updateOrders = (orderID, orders) => {
+    const updatedOrders = orders.filter((order) => order.orderID !== orderID);
+    this.setState({ orders: updatedOrders });
+  };
+
+  updateSales = (order) => {
+    this.setState({ sales: [...this.state.sales, order] });
+  };
+
+  login = (status) => {
+    this.setState({ loggedIn: status });
+  };
+
+  checkLoginStatus = () =>
+    this.state.loggedIn ? (
+      <components.AdminPage
+        addProduct={this.addProduct}
+        addOrder={this.addOrder}
+        updateOrders={this.updateOrders}
+        updateSales={this.updateSales}
+        sales={this.state.sales}
+        orders={this.state.orders}
+      />
+    ) : null;
 
   render() {
     return (
       <div className="App">
         {components.Header()}
+        <components.Login
+          auth={this.state.auth}
+          login={this.login}
+          loggedIn={this.state.loggedIn}
+        />
         <components.Nav />
-        <components.Cart />
+        <components.Cart addOrder={this.addOrder} />
         <components.ProductDisplay products={this.state.products} />
-        <components.AdminPage addProduct={this.addProduct} />
+        {this.checkLoginStatus()}
         <components.Nav />
       </div>
     );
--- src/api/index.js
+++ src/api/index.js
@@ -7,31 +7,4 @@ export const getData = async (url) => {
   }
 };
 
-const getProducts = async () => {
-  try {
-    const resp = await window.fetch(
-      "http://localhost:3001/api/products/products"
-    );
-    return resp.json();
-  } catch (e) {
-    throw new Error(e);
-  }
-};
-
-const getSales = async () => {
-  try {
-    const resp = await window.fetch("http://localhost:3001/api/sales/sales");
-    return resp.json();
-  } catch (e) {
-    throw new Error(e);
-  }
-};
-
-const getAuth = async () => {
-  try {
-    const resp = await window.fetch("http://localhost:3001/api/auth/auth");
-    return resp.json();
-  } catch (e) {
-    throw new Error(e);
-  }
-};
+export const postData = async () => {};
