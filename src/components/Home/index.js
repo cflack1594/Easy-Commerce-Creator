9003cede3dbfbed845cffdabdb992fe3d6d00a56
cflack1594 <cflack1594@gmail.com>
2021-02-01 12:21:54 -0600

    Add app info on home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "bulma/css/bulma.css";
 export class Home extends React.Component {
   heading = "Welcome!";
 
@@ -13,7 +14,20 @@ export class Home extends React.Component {
   }
 
   createSiteList = () => {
-    return <li>Site created with Easy Commerce Creator</li>;
+    return (
+      <ul className="has-text-primary-light">
+        <li>
+          - Site Register tab is a placeholder and is not functional at the
+          moment, pages made with this app will eventually be listed here
+        </li>
+        <li>- Plan to add inventory control system for web page admins</li>
+        <li>
+          - This application is a project created for learning and testing
+          purposes and should not be treated as a site to conduct legitimate
+          business
+        </li>
+      </ul>
+    );
   };
 
   render() {
@@ -23,7 +37,7 @@ export class Home extends React.Component {
           <h1 className="container title is-1  has-text-primary-light">
             {this.heading}
           </h1>
-          <ul>{this.createSiteList()}</ul>
+          {this.createSiteList()}
         </div>
       </div>
     );
